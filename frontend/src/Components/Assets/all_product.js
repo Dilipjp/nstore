d357cd7b6d5c8f059c2dd13b9871c40e132072aa
Dilipjp <jpdilipk@gmail.com>
2024-04-04 18:43:16 +0530

    project uploaded

---
+++ frontend/src/Components/Assets/all_product.js
@@ -0,0 +1,329 @@
+import p1_img from "./product_1.png";
+import p2_img from "./product_2.png";
+import p3_img from "./product_3.png";
+import p4_img from "./product_4.png";
+import p5_img from "./product_5.png";
+import p6_img from "./product_6.png";
+import p7_img from "./product_7.png";
+import p8_img from "./product_8.png";
+import p9_img from "./product_9.png";
+import p10_img from "./product_10.png";
+import p11_img from "./product_11.png";
+import p12_img from "./product_12.png";
+import p13_img from "./product_13.png";
+import p14_img from "./product_14.png";
+import p15_img from "./product_15.png";
+import p16_img from "./product_16.png";
+import p17_img from "./product_17.png";
+import p18_img from "./product_18.png";
+import p19_img from "./product_19.png";
+import p20_img from "./product_20.png";
+import p21_img from "./product_21.png";
+import p22_img from "./product_22.png";
+import p23_img from "./product_23.png";
+import p24_img from "./product_24.png";
+import p25_img from "./product_25.png";
+import p26_img from "./product_26.png";
+import p27_img from "./product_27.png";
+import p28_img from "./product_28.png";
+import p29_img from "./product_29.png";
+import p30_img from "./product_30.png";
+import p31_img from "./product_31.png";
+import p32_img from "./product_32.png";
+import p33_img from "./product_33.png";
+import p34_img from "./product_34.png";
+import p35_img from "./product_35.png";
+import p36_img from "./product_36.png";
+
+let all_product = [
+  {
+    id: 1,
+    name: "Nike Air Max 1",
+    category: "women",
+    image: p1_img,
+    new_price: 160.0,
+    old_price: 180.5,
+  },
+  {
+    id: 2,
+    name: "Nike Air Max Plus",
+    category: "women",
+    image: p2_img,
+    new_price: 185.0,
+    old_price: 195.5,
+  },
+  {
+    id: 3,
+    name: "Nike Air Max 1 '87",
+    category: "women",
+    image: p3_img,
+    new_price: 180.0,
+    old_price: 222.5,
+  },
+  {
+    id: 4,
+    name: "Nike Air Max Plus",
+    category: "women",
+    image: p4_img,
+    new_price: 235.0,
+    old_price: 240.0,
+  },
+  {
+    id: 5,
+    name: "Nike Air Max INTRLK Lite",
+    category: "women",
+    image: p5_img,
+    new_price: 105.0,
+    old_price: 120.5,
+  },
+  {
+    id: 6,
+    name: "Nike Air Max Furyosa",
+    category: "women",
+    image: p6_img,
+    new_price: 147.99,
+    old_price: 210.5,
+  },
+  {
+    id: 7,
+    name: "Nike Air Max 95 By You",
+    category: "women",
+    image: p7_img,
+    new_price: 235.0,
+    old_price: 255.5,
+  },
+  {
+    id: 8,
+    name: "Nike Air Max 1 '86 OG G ",
+    category: "women",
+    image: p8_img,
+    new_price: 210.0,
+    old_price: 222.5,
+  },
+  {
+    id: 9,
+    name: "Nike Air Max 1",
+    category: "women",
+    image: p9_img,
+    new_price: 180.0,
+    old_price: 196.5,
+  },
+  {
+    id: 10,
+    name: "Nike Air Max 1 '87",
+    category: "women",
+    image: p10_img,
+    new_price: 210.0,
+    old_price: 220.5,
+  },
+  {
+    id: 11,
+    name: "Nike Air Max INTRLK Lite",
+    category: "women",
+    image: p11_img,
+    new_price: 115.0,
+    old_price: 129.5,
+  },
+  {
+    id: 12,
+    name: "Nike Air Max Furyosa NRG",
+    category: "women",
+    image: p12_img,
+    new_price: 147.0,
+    old_price: 152.5,
+  },
+  {
+    id: 13,
+    name: "Nike Air Max Dn",
+    category: "men",
+    image: p13_img,
+    new_price: 210.0,
+    old_price: 250.5,
+  },
+  {
+    id: 14,
+    name: "Nike Air Max Dn",
+    category: "men",
+    image: p14_img,
+    new_price: 220.0,
+    old_price: 260.5,
+  },
+  {
+    id: 15,
+    name: "Nike Air Max Dn",
+    category: "men",
+    image: p15_img,
+    new_price: 230.0,
+    old_price: 270.5,
+  },
+  {
+    id: 16,
+    name: "Nike Air Max Dn",
+    category: "men",
+    image: p16_img,
+    new_price: 225.0,
+    old_price: 265.5,
+  },
+  {
+    id: 17,
+    name: "Nike Air Max Dn",
+    category: "men",
+    image: p17_img,
+    new_price: 235.0,
+    old_price: 255.5,
+  },
+  {
+    id: 18,
+    name: "Nike Air Max Dn",
+    category: "men",
+    image: p18_img,
+    new_price: 245.0,
+    old_price: 250.5,
+  },
+  {
+    id: 19,
+    name: "Nike Air Max Dn",
+    category: "men",
+    image: p19_img,
+    new_price: 230.0,
+    old_price: 260.5,
+  },
+  {
+    id: 20,
+    name: "Nike Air Max Dn",
+    category: "men",
+    image: p20_img,
+    new_price: 226.0,
+    old_price: 261.5,
+  },
+  {
+    id: 21,
+    name: "Nike Air Max Dn",
+    category: "men",
+    image: p21_img,
+    new_price: 215.0,
+    old_price: 242.5,
+  },
+  {
+    id: 22,
+    name: "Nike Air Max Dn",
+    category: "men",
+    image: p22_img,
+    new_price: 219.0,
+    old_price: 239.5,
+  },
+  {
+    id: 23,
+    name: "Nike Air Max Dn",
+    category: "men",
+    image: p23_img,
+    new_price: 211.0,
+    old_price: 231.5,
+  },
+  {
+    id: 24,
+    name: "Nike Air Max Dn",
+    category: "men",
+    image: p24_img,
+    new_price: 217.0,
+    old_price: 216.5,
+  },
+  {
+    id: 25,
+    name: "Nike Air Max Dn",
+    category: "kid",
+    image: p25_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 26,
+    name: "Nike Air Max Dn",
+    category: "kid",
+    image: p26_img,
+    new_price: 89.0,
+    old_price: 120.5,
+  },
+  {
+    id: 27,
+    name: "Nike Air Max Dn",
+    category: "kid",
+    image: p27_img,
+    new_price: 91.0,
+    old_price: 135.5,
+  },
+  {
+    id: 28,
+    name: "Nike Air Max Dn",
+    category: "kid",
+    image: p28_img,
+    new_price: 112.0,
+    old_price: 126.5,
+  },
+  {
+    id: 29,
+    name: "Nike Air Max Dn",
+    category: "kid",
+    image: p29_img,
+    new_price: 136.0,
+    old_price: 142.5,
+  },
+  {
+    id: 30,
+    name: "Nike Air Max Dn",
+    category: "kid",
+    image: p30_img,
+    new_price: 142.0,
+    old_price: 152.5,
+  },
+  {
+    id: 31,
+    name: "Nike Air Max Dn",
+    category: "kid",
+    image: p31_img,
+    new_price: 161.0,
+    old_price: 182.5,
+  },
+  {
+    id: 32,
+    name: "Nike Air Max Dn",
+    category: "kid",
+    image: p32_img,
+    new_price: 172.0,
+    old_price: 191.5,
+  },
+  {
+    id: 33,
+    name: "Nike Air Max Dn",
+    category: "kid",
+    image: p33_img,
+    new_price: 185.0,
+    old_price: 196.5,
+  },
+  {
+    id: 34,
+    name: "Nike Air Max Dn",
+    category: "kid",
+    image: p34_img,
+    new_price: 154.0,
+    old_price: 182.5,
+  },
+  {
+    id: 35,
+    name: "Nike Air Max Dn",
+    category: "kid",
+    image: p35_img,
+    new_price: 166.0,
+    old_price: 191.5,
+  },
+  {
+    id: 36,
+    name: "Nike Air Max Dn",
+    category: "kid",
+    image: p36_img,
+    new_price: 151.0,
+    old_price: 172.5,
+  },
+];
+
+export default all_product;
---
+++ frontend/src/Pages/Cart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div>Cart1</div>
+  )
+}
+
+export default Cart
