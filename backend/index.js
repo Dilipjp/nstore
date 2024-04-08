1cce1063adca52fa7dd8e0328dbe945920ab9b8a
Dilipjp <jpdilipk@gmail.com>
2024-04-09 00:33:08 +0530

    API updated

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,19 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
+// Database
+mongoose.connect("mongodb+srv://dilip:dilip123@cluster0.po2gl1g.mongodb.net/n-store");
+
+// API
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to  N-Store API")
+})
+
+app.listen(port,(error) => {
+    if(!error){
+        console.log("Server running on port "+ port)
+    }else{
+        console.log("Eorror:"+ error);
+    }
+})
