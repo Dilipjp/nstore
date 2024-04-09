0adcf9ca33c095ce5e2841a1a5bd5fd1cbefa03d
Dilipjp <jpdilipk@gmail.com>
2024-04-09 21:54:01 +0530

    image upload issue fixed

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,25 @@ app.get("/",(req,res)=>{
     res.send("Welcome to  N-Store API")
 })
 
+// Images
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage: storage})
+
+// upload image endpoint
+app.use('/images', express.static('upload/images'))
+app.post("/upload", upload.single('product'), (req, res) => {
+   res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`
+   }) 
+})
+
 app.listen(port,(error) => {
     if(!error){
         console.log("Server running on port "+ port)
